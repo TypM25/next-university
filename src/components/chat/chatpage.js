2c722e42c1f35c6424694b31741e5de06d7eeb71
TypM <tapdecotap2512@gmail.com>
2025-06-05 12:41:21 +0700

    m

--- src/components/chat/chatpage.js
+++ src/components/chat/chatpage.js
@@ -52,7 +52,7 @@ export default function ChatPage({ user_sender, user_receiver }) {
   useEffect(() => {
     if (!user_sender) return;
     //หาroom chat
-    socketRef.current = io('http://localhost:8000', {
+    socketRef.current = io('http://localhost:9000', {
       auth: {
         user_id: user_sender,
         chat_partner_id: user_receiver,
